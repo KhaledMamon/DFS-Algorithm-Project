32607f7b040c47c39718908252a4d17315ff9af5
Khaled Mamon <khaledgamal.boy@gmail.com>
2025-12-23 22:26:37 +0200

    Implement data generation for MST testing
    Added a function to generate random building data for testing the MST algorithm.

--- algorithm1.py
+++ algorithm1.py
@@ -1,5 +1,8 @@
 
+import math
+import random
 import sys
+import time
 
 sys.setrecursionlimit(20000)
 
@@ -42,4 +45,39 @@ def run_naive_mst(num_buildings, all_edges):
             count += 1
             
     return mst_edges, mst_cost
+
+
+def generate_data(num_buildings):
+
+    buildings = []
+    for _ in range(num_buildings):
+        buildings.append((random.randint(0, 1000), random.randint(0, 1000)))
     
+    edges = []
+    for i in range(num_buildings):
+        for j in range(i + 1, num_buildings):
+            x1, y1 = buildings[i]
+            x2, y2 = buildings[j]
+            dist = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+            edges.append((i, j, dist))
+    return edges
+
+if __name__ == "__main__":
+    print("="*30)
+    
+    test_sizes = [50, 100, 300, 500]
+    
+    print(f"{'Input Size ':<15} | {'Naive Time ':<15}  ")
+    print("-" * 30)
+    
+    for n in test_sizes:
+        edges = generate_data(n)
+        
+        start = time.time()
+        _, cost = run_naive_mst(n, edges)
+        end = time.time()
+        time0 = end - start
+
+        print(f"{n:<15} | {time0:.6f} ")
+        
+    print("="*30)
